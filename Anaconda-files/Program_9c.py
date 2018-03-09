f39164d35a31bb58de6ab24cc52c764b50dddadb
Ben Nuttall <ben@bennuttall.com>
2018-03-09 23:03:06 +0000

    Add titles and move plt.show() to the end to show both plots together

--- Anaconda-files/Program_9c.py
+++ Anaconda-files/Program_9c.py
@@ -1,5 +1,5 @@
-# Program 9c: Phase portrait and Poincare section of a nonautonomous ODE. 
-# See Figure 9.11(b). 
+# Program 9c: Phase portrait and Poincare section of a nonautonomous ODE.
+# See Figure 9.11(b).
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
@@ -7,29 +7,32 @@ xmin=-2;xmax=2;ymin=-2;ymax=2;
 k=0.3;omega=1.25;Gamma=0.5;
 def dx_dt(x, t):
     return [x[1],x[0]-k*x[1]-x[0]**3+Gamma*np.cos(omega*t)]
- 
+
 # Phase portrait.
 t=np.linspace(0,500,10000)
-xs=odeint(dx_dt,[1,0],t) 
+xs=odeint(dx_dt,[1,0],t)
 plt.plot(xs[:,0],xs[:,1], "r-",lw=0.5)
-plt.xlabel("x",fontsize=15) 
-plt.ylabel("y",fontsize=15) 
-plt.tick_params(labelsize=15) 
-plt.xlim(xmin,xmax) 
+plt.xlabel("x",fontsize=15)
+plt.ylabel("y",fontsize=15)
+plt.tick_params(labelsize=15)
+plt.xlim(xmin,xmax)
 plt.ylim(ymin,ymax);
-plt.show()
+plt.title("Phase portrait")
 
 # The Poincare section.
 x=[];y=[];
 
 fig, ax = plt.subplots(figsize=(6,6))
 t=np.linspace(0,4000*(2*np.pi)/omega,16000000)
-xs=odeint(dx_dt,[1,0],t) 
+xs=odeint(dx_dt,[1,0],t)
 for i in range(4000):
     x.extend([xs[4000*i,0]])
     y.extend([xs[4000*i,1]])
-    
+
 ax.scatter(x,y, color='blue',s=0.1)
-plt.xlabel("x",fontsize=15) 
-plt.ylabel("y",fontsize=15) 
-plt.tick_params(labelsize=15) 
+plt.xlabel("x",fontsize=15)
+plt.ylabel("y",fontsize=15)
+plt.tick_params(labelsize=15)
+plt.title("The Poincare section")
+
+plt.show()
