7dbd2fca0150876c604b0bbe9358290035a0a867
Ben Nuttall <ben@bennuttall.com>
2018-03-10 11:49:43 +0000

    Add missing plt.show()

--- Anaconda-files/Programs_17a.py
+++ Anaconda-files/Programs_17a.py
@@ -10,20 +10,21 @@ x = [0]*(N_lines+1)
 y = [0]*(N_lines+1)
 x[0] = 0
 y[0] = 0
-                                        
+
 segment=[0]*N_lines;
-        
-# The angles of the four segments. 
-angle=[0,np.pi/3,-np.pi/3,0]   
+
+# The angles of the four segments.
+angle=[0,np.pi/3,-np.pi/3,0]
 for i in range(N_lines):
     m=i;ang=0;
     for j in range(k):
         segment[j]=np.mod(m,4)
         m=floor(m/4)
         ang=ang+angle[segment[j]]
-        
+
     x[i+1]=x[i]+h*np.cos(ang)
     y[i+1]=y[i]+h*np.sin(ang)
 
 plt.axis('equal')
-plt.plot(x,y)  
+plt.plot(x,y)
+plt.show()
