924bd2aab203cac22ee60a0141bc7cc814b42d7d
Ben Nuttall <ben@bennuttall.com>
2018-03-10 14:04:30 +0000

    Add missing plt.show()

--- Anaconda-files/Programs_19c.py
+++ Anaconda-files/Programs_19c.py
@@ -7,7 +7,7 @@ from scipy.integrate import odeint
 
 # Constants
 sigma=16;b=4;r=45.92;
-Tmax=100;       
+Tmax=100;
 
 t=np.arange(0.0, Tmax, 0.1)
 
@@ -15,11 +15,11 @@ def Two_Lorenz_ODEs(X,t):
     x1,x2,x3,y2,y3=X
     dx1 = sigma*(x2-x1)
     dx2 = -x1*x3+r*x1-x2
-    dx3 = x1*x2-b*x3          
+    dx3 = x1*x2-b*x3
     dy2 = -x1*y3+r*x1-y2
     dy3 = x1*y2-b*y3
     return dx1,dx2,dx3,dy2,dy3
-    
+
 X = odeint(Two_Lorenz_ODEs,[15,20,30,10,20],t,rtol=1e-6)
 x1 = X[:,0];x2 = X[:,1];x3 = X[:,2];
 y2 = X[:,3];y3 = X[:,4];
@@ -29,3 +29,4 @@ plt.figure(1)
 plt.plot(x3,y3)
 plt.xlabel(r'$y_3$',fontsize=15)
 plt.ylabel(r'$x_3$',fontsize=15)
+plt.show()
