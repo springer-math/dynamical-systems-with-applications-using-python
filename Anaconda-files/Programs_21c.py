8ee30ec28139d32150e2eb654ae5bb85d11e8cf1
Ben Nuttall <ben@bennuttall.com>
2018-03-10 15:00:36 +0000

    Move plt.show() to the end to ensure labels are included

--- Anaconda-files/Programs_21c.py
+++ Anaconda-files/Programs_21c.py
@@ -1,4 +1,4 @@
-# Programs 21c: Josephson junction limit cycle. 
+# Programs 21c: Josephson junction limit cycle.
 # See Figure 21.9.
 from matplotlib import pyplot as plt
 import numpy as np
@@ -9,12 +9,12 @@ BJ=1.2;Tmax=100;kappa=1.4;
 def JJ_ODE(x, t):
     return [x[1],kappa-BJ*x[1]-np.sin(x[0])]
 
-time = np.arange(0, Tmax, 0.1) 
+time = np.arange(0, Tmax, 0.1)
 x0=[0.1,0.1]
 xs = odeint(JJ_ODE, x0, time)
 imgplot = plt.plot(np.sin(xs[:,0]), xs[:,1], "r-")
 
-plt.show()
 plt.xlabel(r'$\sin(\phi)$',fontsize=15)
 plt.ylabel(r'$\Omega$',fontsize=15)
 plt.tick_params(labelsize=15)
+plt.show()
