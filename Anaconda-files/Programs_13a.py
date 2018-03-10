7e21cff326c74e210a0b6d21e658ef1b91c229e9
Ben Nuttall <ben@bennuttall.com>
2018-03-10 00:01:59 +0000

    Use string formatting to better present the solutions

--- Anaconda-files/Programs_13a.py
+++ Anaconda-files/Programs_13a.py
@@ -4,6 +4,6 @@ from sympy.abc import n
 x=Function('x');
 f=x(n+1)-(1+3/100)*x(n);
 sol=rsolve(f,x(n),{x(0):10000});
-print('x_n=',sol)
+print('x_n = {}'.format(sol))
 x_5=round(sol.subs(n,5),2)
-print('x(5)=£',x_5)
+print('x(5) = £{:,}'.format(x_5))
